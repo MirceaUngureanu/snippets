95c6f578ac5292f7f7d808bc83d6e0c98cd547b0
Mircea Ungureanu <mircea.ungureanu@codecomputerlove.com>
2019-06-14 08:39:00 +0100

    unit test template

---
+++ unit-test-template.js
@@ -0,0 +1,12 @@
+import test from "tape";
+
+// For each unit test you write,
+// answer these questions:
+test("What component aspect are you testing?", assert => {
+  const actual = "What is the actual output?"; // will also describe how the test can be reproduced
+  const expected = "What is the expected output?";
+
+  assert.equal(actual, expected, "What should the feature do?");
+
+  assert.end();
+});
